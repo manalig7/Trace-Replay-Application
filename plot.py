f8f1dc46328f5014d4a1f1a79e6e4c2806ff9c48
Harita Reddy <harita_1997@yahoo.com>
2017-11-10 18:28:15 +0530

    Update plot.py

--- plot.py
+++ plot.py
@@ -2,9 +2,8 @@ import matplotlib.pyplot as plt
 
 time=[]
 nbytes=[]
-
-with open('videodownloadinput.txt') as fp:  
-   line = fp.readline()
+fp=open('videodownloadinput.txt')
+for line in fp:
    ls=line.split(" ")
    time.append(ls[0])
    nbytes.append(ls[1])
